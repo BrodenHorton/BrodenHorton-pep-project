626a35b0f91e90852374a1734990ff13b7ccc448
Broden Horton <brody_horton@hotmail.com>
2025-03-23 18:01:17 +0000

    Implemented first task, insert account.

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -2,6 +2,11 @@ package Controller;
 
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import Model.Account;
+import Service.AccountService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
@@ -9,6 +14,12 @@ import io.javalin.http.Context;
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
+    private AccountService accountService;
+
+    public SocialMediaController() {
+        accountService = new AccountService();
+    }
+
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
@@ -17,6 +28,8 @@ public class SocialMediaController {
     public Javalin startAPI() {
         Javalin app = Javalin.create();
         app.get("example-endpoint", this::exampleHandler);
+        
+        app.post("/register", this::postAccountHandler);
 
         return app;
     }
@@ -27,6 +40,18 @@ public class SocialMediaController {
      */
     private void exampleHandler(Context context) {
         context.json("sample text");
+    }
+
+    private void postAccountHandler(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Account account = mapper.readValue(ctx.body(), Account.class);
+        Account addedAccount = accountService.addAccount(account);
+        if(addedAccount != null) {
+            ctx.json(addedAccount);
+        }
+        else {
+            ctx.status(400);
+        }
     }
 
 
---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,95 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class AccountDAO {
+
+    public List<Account> getAllAccounts() {
+        List<Account> accounts = new ArrayList<>();
+        Connection conn = ConnectionUtil.getConnection();
+
+        try {
+            String sql = "SELECT * FROM account";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()) {
+                Account account = new Account();
+                account.setAccount_id(resultSet.getInt("account_id"));
+                account.setUsername(resultSet.getString("username"));
+                account.setPassword(resultSet.getString("password"));
+                accounts.add(account);
+            }
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return accounts;
+    }
+
+    public Account getAccountByUsername(String username) {
+        Connection conn = ConnectionUtil.getConnection();
+
+        try {
+            String sql = "SELECT * FROM account WHERE username = ?";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+
+            preparedStatement.setString(1, username);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()) {
+                Account account = new Account();
+                account.setAccount_id(resultSet.getInt("account_id"));
+                account.setUsername(resultSet.getString("username"));
+                account.setPassword(resultSet.getString("password"));
+                return account;
+            }
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+    }
+
+    public Account insertAccount(Account account) {
+        if(account.getUsername() == null || account.getUsername().length() == 0 || getAccountByUsername(account.getUsername()) != null) {
+            return null;
+        }
+        if(account.getPassword().length() < 4) {
+            return null;
+        }
+
+        Connection conn = ConnectionUtil.getConnection();
+
+        try {
+            String sql = "INSERT INTO account (username, password) VALUES(?, ?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            preparedStatement.setString(1, account.getUsername());
+            preparedStatement.setString(2, account.getPassword());
+
+            preparedStatement.executeUpdate();
+            ResultSet pKey = preparedStatement.getGeneratedKeys();
+            if(pKey.next()) {
+                return new Account((int)pKey.getLong(1), account.getUsername(), account.getPassword());
+            }
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+    }
+}
